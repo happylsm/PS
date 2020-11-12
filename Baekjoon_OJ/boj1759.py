9222b2b3dcf3f74158f05704e783fec7ada1c512
seungmin <48234814+happylsm@users.noreply.github.com>
2020-11-12 22:29:45 +0900

    Create boj1759.py

---
+++ Baekjoon_OJ/boj1759.py
@@ -0,0 +1,19 @@
+# combination 안쓰고 해보기   
+
+import sys 
+from itertools import combinations
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+L, C = map(int,input().split(' '))
+pwd = sorted(input().split(' ')[:C]) 
+
+comb = combinations(pwd, L)
+
+for c in comb:
+    cnt = 0
+    for letter in c:
+        if letter in vowels:
+            cnt += 1
+
+    if cnt >= 1 and cnt <= L-2:
+        print(''.join(c))
